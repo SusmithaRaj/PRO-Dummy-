755269880d21dc3dcdcb363529c1a0dfdfde36ce
SusmithaRaj <36538000+SusmithaRaj@users.noreply.github.com>
2019-10-30 14:48:10 +0530

    Add files via upload

---
+++ emailtest.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST['name']) && isset($_POST['email'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $to = 'susmitharaj30@gmail.com';
+    $subject = "New Message on KristjanMark.com";
+    $body = '<html>
+                <body>
+                    <h2>Title</h2>
+                    <br>
+                    <p>Name:<br>'.$name.'</p>
+                    <p>Email:<br>'.$email.'</p>
+
+                </body>
+            </html>';
+
+//headers
+$headers = "From: ".$name." <".$email.">\r\n";
+$headers = "Reply-To: ".$email."\r\n";
+$headers = "MIME-Version: 1.0\r\n";
+$headers = "Content-type: text/html; charset=utf-8";
+
+//send
+$send = mail($to, $subject, $body, $headers);
+if ($send) {
+    echo '<br>';
+    echo "Success. Thanks for Your Message.";
+} else {
+    echo 'Error.';
+}
+}
+?>
